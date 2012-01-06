5a90641d04f710825d216753d4ade67d265c1bb0
Josh Crews <crews.josh@gmail.com>
2012-01-05 19:49:30 -0600

    DRY file names in rspec template tests

--- spec/templates/admin_layout_navigation_template_spec.rb
+++ spec/templates/admin_layout_navigation_template_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "navigation.html.erb" do
   
   before(:all) do
-    @file =  File.expand_path('../../../lib/generators/flexible_admin/templates/navigation.html.erb', __FILE__)
+    @file =  File.expand_path("../../../lib/generators/flexible_admin/templates/#{self.class.description}", __FILE__)
   end
   
   it "should link to admin home" do
