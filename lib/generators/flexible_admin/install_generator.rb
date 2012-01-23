b2b2158a1d8d64fcb9097fbe56bc71abb4110815
Josh Crews <crews.josh@gmail.com>
2012-01-23 12:57:16 -0600

    move devise out of install generator

--- lib/generators/flexible_admin/install_generator.rb
+++ lib/generators/flexible_admin/install_generator.rb
@@ -82,32 +82,5 @@ module FlexibleAdmin
       gem 'inherited_resources'
     end
     
-    def devise
-      unless defined?(Devise)
-        say "Adding devise gem to your Gemfile:"
-        append_file "Gemfile", "\n", :force => true
-        gem 'devise'
-      end
-      
-      inside Rails.root do
-        run "bundle install"
-      end        
-      
-      unless File.exists?(Rails.root.join("config/initializers/devise.rb"))
-        say "Installing devise"
-        generate "devise:install"
-      end
-      
-      unless File.exists?(Rails.root.join("app/models/admin_user.rb"))
-        generate "devise AdminUser"
-        template "user_sign_in.html.erb", "app/views/admin/admin_users/sessions/new.html.erb"
-        gsub_file 'config/routes.rb', "devise_for :admin_users", "devise_for :admin_users, :path_prefix => 'admin', :controllers => {:sessions => 'admin/admin_users/sessions' }"
-        template "admin_user_sessions_controller.rb", "app/controllers/admin/admin_users/sessions_controller.rb"
-      end
-      
-      say "you now need to run 'rake db:migrate' to create the admin_users table", :blue
-      say "after that make your first admin_user in the console: AdminUser.create!(email: 'your_email@gmail.com', password: 'password')", :blue
-    end
-
   end
 end
